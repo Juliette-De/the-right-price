de579c6d84910657434925dddc3333beff53a29c
Augustin de la Brosse <augustin2164@gmail.com>
2023-01-31 14:32:52 +0100

    improved function + EDA

--- server_configuration/app.py
+++ server_configuration/app.py
@@ -21,7 +21,7 @@ geo_IDF_sample = gpd.GeoDataFrame(
     IDF_sample, geometry=gpd.points_from_xy(IDF_sample.longitude, IDF_sample.latitude))
 
 # Parameters
-st.subheader('Map of all mutation')
+st.subheader('Map of all mutations')
 
 dept_slider = st.selectbox(
     'Select a department',
@@ -44,6 +44,48 @@ property_slider = st.selectbox(
     'Select a type of property',
     [None] + property_list)
 
+
+i = IDF_sample['valeurfonc'].min()
+a = 10**(len(str(int(i)))-1)
+min_value = a*np.floor(i/a) if i > 1 else 0.0
+j = IDF_sample['valeurfonc'].max()
+b = 10**(len(str(int(j)))-1)
+max_value = b*np.ceil(j/b) if j > 1 else 0.0
+
+luxury = st.checkbox('Check this box to see the most expansive properties')
+
+if luxury:
+    min_value_slider, max_value_slider = st.select_slider(
+        'Select a property value', 
+        np.arange(1500000, max_value+1, 1000), 
+        value=(1500000, max_value))
+else:
+    min_value_slider, max_value_slider = st.select_slider(
+        'Select a property value', 
+        np.arange(min_value, 1500001, 1000), 
+        value=(min_value, 1500000))
+    
+    
+k = IDF_sample['sbati'].min()
+c = 10**(len(str(int(k)))-1)
+min_surface = c*np.floor(k/c) if k > 1 else 0.0
+l = IDF_sample['sbati'].max()
+d = 10**(len(str(int(l)))-1)
+max_surface = d*np.ceil(l/d) if l > 1 else 0.0
+
+if luxury:
+    min_surface_slider, max_surface_slider = st.select_slider(
+        'Select a surface', 
+        np.arange(min_surface, max_surface+1, 10), 
+        value=(min_surface, max_surface))
+else:
+    min_surface_slider, max_surface_slider = st.select_slider(
+        'Select a surface', 
+        np.arange(min_surface, 1501, 10), 
+        value=(min_surface, 1500))
+
+
+
 year_list = IDF_sample['year'].unique().tolist()
 year_list.sort()
 year_slider = st.selectbox(
@@ -51,8 +93,19 @@ year_slider = st.selectbox(
     [None] + year_list)
 
 
-folium_map = plot_map(geo_IDF_sample, dept=dept_slider, insee=insee_slider, section=section_slider, type_of_property=property_slider, year_of_mutation=year_slider)
-folium_static(folium_map)
+folium_map = plot_map(geo_IDF_sample, 
+                      dept=dept_slider, 
+                      insee=insee_slider, 
+                      section=section_slider, 
+                      type_of_property=property_slider, 
+                      year_of_mutation=year_slider, 
+                      min_value=min_value_slider, max_value=max_value_slider,
+                      min_surface=min_surface_slider, max_surface=max_surface_slider)
+
+if folium_map is None:
+    st.subheader('There are no mutations that meet your criterias')
+else:
+    folium_static(folium_map)
 
 # hour_to_filter = st.slider('hour', 0, 31, 17)  # min: 0h, max: 23h, default: 17h
 # filtered_data = sample_IDF[sample_IDF['day'].dt.hour == hour_to_filter]
--- server_configuration/helpers.py
+++ server_configuration/helpers.py
@@ -5,7 +5,7 @@ import folium
 import re
 
 
-def plot_map(geo_df, dept=None, insee=None, section=None, type_of_property=None, year_of_mutation=None):
+def plot_map(geo_df, dept=None, insee=None, section=None, type_of_property=None, year_of_mutation=None, max_value=None, min_value=None, min_surface=None, max_surface=None):
     
     """
     Plots a map showing the locations of properties based on the given parameters
@@ -17,9 +17,14 @@ def plot_map(geo_df, dept=None, insee=None, section=None, type_of_property=None,
     section (str, optional): Property section. Defaults to None.
     type_of_property (str, optional): Type of property. Defaults to None.
     year_of_mutation (int, optional): Year of mutation. Defaults to None.
+    max_value (int, optional): Maximum value of property to filter data by, default is None
+    min_value (int, optional): Minimum value of property to filter data by, default is None
+    min_surface (int, optional): Minimum surface of property to filter data by, default is None
+    max_surface (int, optional): Maximum surface of property to filter data by, default is None
 
     Returns:
-    folium.Map: A map object showing the locations of the properties. Returns None if the criteria selected result in no data.
+    None: If no data meets criteria or if municipality is not in selected department
+    folium.Map: Interactive map of filtered properties with markers showing property details
     """
 
     
@@ -44,11 +49,35 @@ def plot_map(geo_df, dept=None, insee=None, section=None, type_of_property=None,
     
     if year_of_mutation is not None:
         geo_zone = geo_zone[geo_zone['year'] == year_of_mutation]
+        
+    if max_value is not None and min_value is not None:
+        if min_value <= max_value:
+            geo_zone = geo_zone[(geo_zone['valeurfonc'] >= min_value) & (geo_zone['valeurfonc'] <= max_value)]
+        else:
+            print("The maximum value cannot be lower than the minimum value. Please change these creteria.")
+            return None
+    elif max_value is None and min_value is not None:
+        geo_zone = geo_zone[geo_zone['valeurfonc'] >= min_value]
+    elif max_value is not None and min_value is None:
+        geo_zone = geo_zone[geo_zone['valeurfonc'] <= max_value]
+        
+    if min_surface is not None and max_surface is not None:
+        if min_surface <= max_surface:
+            geo_zone = geo_zone[(geo_zone['sbati'] >= min_surface) & (geo_zone['sbati'] <= max_surface)]
+        else:
+            print("The maximum surface cannot be lower than the minimum surface. Please change these creteria.")
+            return None
+    elif max_surface is None and min_surface is not None:
+        geo_zone = geo_zone[geo_zone['sbati'] >= min_surface]
+    elif max_surface is not None and min_surface is None:
+        geo_zone = geo_zone[geo_zone['sbati'] <= max_surface]
 
+        
     geo_df_list = [(i[0], i[1]) for i in geo_zone[['latitude', 'longitude']].values]
     if len(geo_df_list) == 0:
         print("There are no mutation that meet the criteria you have selected. Please change these creteria.")
         return None    
+    
     
     map = folium.Map(location=[geo_zone.latitude.median(), geo_zone.longitude.median()], tiles="OpenStreetMap")
     for i, coordinates in enumerate(geo_df_list):
@@ -57,8 +86,10 @@ def plot_map(geo_df, dept=None, insee=None, section=None, type_of_property=None,
                 location=coordinates,
                 popup=
                     "Nature de la mutation: " + str(geo_zone.libnatmut.values[i]) + "<br>" + "<br>"
+                    "Valeur foncière: " + str(geo_zone.valeurfonc.values[i]) + "<br>" + "<br>"
                     f"Date de la mutation: {geo_zone.day.values[i]}/{geo_zone.month.values[i]}/{geo_zone.year.values[i]}" + "<br>" + "<br>"
                     "Type de bien: " + str(geo_zone.libtypbien.values[i]) + "<br>" + "<br>"
+                    f"Surface du bien: {str(geo_zone.sbati.values[i])} m^2" + "<br>" + "<br>"
                     "CODE INSEE: " + ', '.join(re.findall(r'[0-9]+', geo_zone.l_codinsee.values[i])) + "<br>" + "<br>"
                     "Section: " + ', '.join(re.findall(r'[0-9A-Z]+', geo_zone.l_section.values[i])) + "<br>" # + "<br>"
                     #+ "Coordinates: " + str(geo_df_list[i]),
