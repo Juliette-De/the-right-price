a16824516b106a3e66a950267b5d9c0d721886b6
Augustin de la Brosse <augustin2164@gmail.com>
2023-02-02 21:02:16 +0100

    added docstrings to the functions

--- server_configuration/helpers.py
+++ server_configuration/helpers.py
@@ -144,7 +144,18 @@ class plotting_functions:
 
 
     def plot_yearly_figures(self, df):
-        
+"""
+    This function plots the yearly figures of property values and number of mutations based on the given dataframe and user-defined criteria. 
+    The criteria include department, municipality, section, type of property, years, value, and surface area. 
+    The function groups the data by year and plots the sum of property values and the number of mutations.
+
+    Parameters:
+        df (DataFrame) : The input dataframe with property data.
+
+    Returns:
+        None
+"""
+
         if self.dept is not None and self.insee is not None:
             if np.floor(self.insee/1000) != self.dept:
                 print("The municipality you chose is not in the department you chose. Please change either the department or the municipality.")
@@ -222,20 +233,20 @@ class plotting_functions:
     def plot_prediction_map(self, geo_df, luxury=False):
 
         """
-        Plots a map showing the locations of properties based on the given parameters
+        Plots a map showing the locations of the cheapest and most expansives properties based on the given parameters
 
         Args:
-        geo_df (geopandas.geodataframe.GeoDataFrame): A dataframe containing property information.
-        dept (int, optional): Department number. Defaults to None.
-        insee (int, optional): Municipality code. Defaults to None.
-        section (str, optional): Property section. Defaults to None.
-        type_of_property (str, optional): Type of property. Defaults to None.
-        min_surface (int, optional): Minimum surface of property to filter data by, default is None
-        max_surface (int, optional): Maximum surface of property to filter data by, default is None
+            geo_df (geopandas.geodataframe.GeoDataFrame): A dataframe containing property information.
+            dept (int, optional): Department number. Defaults to None.
+            insee (int, optional): Municipality code. Defaults to None.
+            section (str, optional): Property section. Defaults to None.
+            type_of_property (str, optional): Type of property. Defaults to None.
+            min_surface (int, optional): Minimum surface of property to filter data by, default is None
+            max_surface (int, optional): Maximum surface of property to filter data by, default is None
 
         Returns:
-        None: If no data meets criteria or if municipality is not in selected department
-        folium.Map: Interactive map of filtered properties with markers showing property details
+            None: If no data meets criteria or if municipality is not in selected department
+            folium.Map: Interactive map of filtered properties with markers showing property details
         """
 
         if self.dept is not None and self.insee is not None:
@@ -328,6 +339,12 @@ class modelling:
         self.folder_path = folder_path
         
     def sort_csv_files(self):
+        """
+        Sort all the .csv files in the folder path specified in the class.
+
+        Returns:
+            A single pandas DataFrame with data from all the .csv files in the folder path.
+        """
         files = [f for f in os.listdir(self.folder_path) if f.endswith('.csv')]
         dfs = []
         for file in files:
@@ -337,6 +354,22 @@ class modelling:
 
     @staticmethod
     def exclude_lines(df, max_fonc=1500000):
+        """
+        Filter dataframe `df` based on certain conditions.
+
+        This function filters the input dataframe `df` based on the following conditions:
+        - `valeurfonc` column should be less than or equal to `max_fonc`
+        - `valeurfonc` column should be greater than or equal to 100
+        - `libtypbien` column should contain specific values as defined in the function
+
+        Args:
+            df (pandas.DataFrame): The dataframe to filter.
+            max_fonc (int): The maximum value for the `valeurfonc` column. The default is 1500000.
+
+        Returns:
+            pandas.DataFrame: The filtered dataframe that meets the conditions defined in the function.
+        """
+        
         res = df[df["valeurfonc"] <= max_fonc]
         res = res[res["valeurfonc"] >= 100]
         res = res[((res["libtypbien"] == 'UN APPARTEMENT')
@@ -350,18 +383,36 @@ class modelling:
 
     @staticmethod
     def add_arrondissement(df):
+        """
+        Add arrondissement column to the dataframe
+
+        This function takes a dataframe as input and adds a new column "arro" to it, which represents the arrondissement of a property. 
+        The arrondissement is derived from the "l_codinsee" column of the dataframe. 
+        If the first two digits of the "l_codinsee" value is equal to "75", the last two digits are considered as the arrondissement. 
+        Else, the arrondissement value is set to 0. The resulting "arro" column is of integer type.
+
+        Args:
+            df (pandas.DataFrame): The input dataframe
+
+        Returns:
+            None
+        """
         n_col = df["l_codinsee"].apply(lambda s: s[-4:-2] if s[2:4]=="75" else 0)
         df["arro"] = n_col.astype("int")
 
-    # @staticmethod
-    # def sum_surface(df):
-    #     df["surf"] = (df["sbati"] 
-    #                   + df["sbatmai"] 
-    #                   + df["sbatapt"]
-    #                   + df["sbatact"])
-
     @staticmethod
     def feature_selection(df):
+        """
+        This function selects the relevant features from the input dataframe and returns a new dataframe
+        with the selected features.
+
+        Parameters:
+            df (pandas.DataFrame): The input dataframe to perform feature selection on
+
+        Returns:
+            pandas.DataFrame: The new dataframe with the selected features
+
+        """
         base_columns = ["anneemut", "moismut", "coddep", "vefa", "valeurfonc", "sterr",
                         "nblocdep", "latitude", "longitude", "nb_accomodations", "surf",
                         "arro", "nb_rooms"
@@ -385,33 +436,78 @@ class modelling:
     
     @staticmethod
     def create_min_max_scaler():
+        """
+        Create Min Max Scaler
+
+        Returns:
+            StandardScaler: instance of StandardScaler from scikit-learn library.
+        """
         scaler = StandardScaler()
         return scaler
 
     @staticmethod
     def min_max_scale(df, scaler):
+        """
+        Transform the values in a single column of a pandas DataFrame using a StandardScaler object.
+
+        Parameters:
+            df (pandas DataFrame): The DataFrame to be transformed.
+            scaler (StandardScaler): The StandardScaler object to use for the transformation.
+
+        Returns:
+            None
+        """        
+
         df['valeurfonc'] = scaler.fit_transform(df['valeurfonc'].values.reshape(-1, 1))
 
     @staticmethod
     def min_max_rescale(list_, scaler):
+        """
+        Rescale a list using the specified MinMaxScaler
+
+        Parameters:
+            list_ (list): List to be rescaled
+            scaler (StandardScaler): Scaler used for the rescale
+
+        Returns
+            rescaled_list (list):Rescaled list
+        """     
         list_ = scaler.inverse_transform(list_.reshape(-1, 1))
         return list_
     
     @staticmethod
     def X_y_split(df):
+        """
+        Splits the data into feature and target variables.
+
+        Inputs:
+            df : pandas dataframe
+            A dataframe with the data to be split into features and target variables.
+
+        Outputs:
+            X (pandas dataframe): A dataframe with feature variables.
+            y (pandas series): A series with target variable.
+    
+        """
         features = ["anneemut", "moismut", "coddep", "vefa", "sterr",
                         "nblocdep", "latitude", "longitude", "nb_accomodations", "surf",
                         "arro", "nb_rooms"
                        ]
-        # ["anneemut", "moismut", "nblot", "sterr", "nbvolmut", "nblocmut",
-        #             "nblocmai", "nblocapt", "nblocdep", "nblocact", "latitude", "longitude",
-        #             "arro", "surf"
-        #            ]
         X = df[features]
         y = df["valeurfonc"]
         return X, y
     
     def preprocessing_and_splitting(self):
+        """
+        Preprocesses the mutation data and splits it into train and test sets
+
+        Returns:
+            X_train (pandas.DataFrame): Training features data.
+            X_test (pandas.DataFrame): Testing features data.
+            Y_train (pandas.Series): Training target data.
+            Y_test (pandas.Series): Testing target data.
+            scaler (StandardScaler): Scaler instance used to scale the "valeurfonc" feature.
+        """        
         df_1 = self.sort_csv_files()
         mutation = self.exclude_lines(df_1)
         self.add_arrondissement(mutation)
@@ -425,6 +521,19 @@ class modelling:
 
     @staticmethod
     def training(X_train, Y_train, save=False, pkl_file_path=None): 
+        """
+        Train an XGBoost regressor model using the provided training data.
+
+        Parameters:
+            X_train (pandas.DataFrame): The training data for the model's independent variables.
+            Y_train (pandas.Series): The training data for the model's dependent variable.
+            save (bool, optional): Whether to save the trained model to a file. Defaults to False.
+            pkl_file_path (str, optional): The file path to save the trained model to, if save is True.
+                Defaults to None.
+
+        Returns:
+            xgboost (xgboost.XGBRegressor): The trained XGBoost model.
+        """
         from xgboost import XGBRegressor
         xgboost = XGBRegressor()
         xgboost.fit(X_train, Y_train)
@@ -434,11 +543,30 @@ class modelling:
 
     @staticmethod
     def predict(xgboost, X_test):
+        """
+        make predictions using an XGBoost model
+
+        Parameters:
+            xgboost (XGBRegressor): Trained XGBoost model
+            X_test (DataFrame): Test data features
+
+        Returns:
+            y_pred (ndarray): Predictions
+        """
         y_pred = xgboost.predict(X_test)
         return y_pred
 
     @staticmethod
     def load_model(pkl_file_path):
+        """
+        Load an XGBoost model saved as a pickle file.
+
+        Parameters:
+            pkl_file_path (str): The file path of the pickled XGBoost model.
+
+        Returns:
+            xgboost (XGBRegressor): The loaded XGBoost model.
+        """
         xgboost = joblib.load(pkl_file_path)
         return xgboost
     
